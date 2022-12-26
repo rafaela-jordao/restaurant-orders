6106d6e176aaced9c6a7a7a6d7d815ec526bd614
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-12-26 14:24:44 -0300

    feat: implementa requisitos 1 e 2 (campanha de publicidade / análises contínuas)

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,37 @@
-# iniciando o projeto
+import csv
+from src.track_orders import TrackOrders
 
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    track = TrackOrders()
+    if ".csv" not in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+    try:
+        with open(path_to_file, encoding="utf-8") as file:
+            content = csv.reader(file)
+            for customer, order, day in content:
+                track.add_new_order(customer, order, day)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
+
+    """ A lanchonete quer promover ações de marketing e,
+    para isso, a agência de publicidade precisa das informações abaixo:
+    Qual o prato mais pedido por 'maria'?
+    Quantas vezes 'arnaldo' pediu 'hamburguer'?
+    Quais pratos 'joao' nunca pediu?
+    Quais dias 'joao' nunca foi à lanchonete?
+    """
+    maria_requests = track.get_most_ordered_dish_per_customer("maria")
+    arnaldo_hamburguer = track.get_quantity_per_customer(
+        "arnaldo", "hamburguer"
+    )
+    joao_not_requests = track.get_never_ordered_per_customer("joao")
+    joao_not_visit = track.get_days_never_visited_per_customer("joao")
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(
+            f"{maria_requests}\n"
+            f"{arnaldo_hamburguer}\n"
+            f"{joao_not_requests}\n"
+            f"{joao_not_visit}\n"
+        )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,57 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self._data = list()
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._data.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer = [
+            order[1] for order in self._data if order[0] == customer
+        ]
+        return Counter(customer).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        dishes = set(order[1] for order in self._data)
+        client = set(
+            order[1]
+            for order in self._data if order[0] == customer
+        )
+        result = dishes - client
+        return result
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days = set(order[2] for order in self._data)
+        client = set(
+            order[2]
+            for order in self._data if order[0] == customer
+        )
+        result = days - client
+        return result
 
     def get_busiest_day(self):
-        pass
+        busiest = Counter(
+            order[2] for order in self._data
+        ).most_common(1)[0][0]
+        return busiest
 
     def get_least_busy_day(self):
-        pass
+        least_busy = Counter(
+            order[2] for order in self._data
+        ).most_common()[-1][0]
+        return least_busy
+
+    def get_quantity_per_customer(self, customer, dish):
+        customer = [
+            order[1]
+            for order in self._data
+            if order[0] == customer and order[1] == dish
+        ]
+        return len(customer)
